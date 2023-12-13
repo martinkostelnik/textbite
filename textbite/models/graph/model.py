14c98021e05b9d91ac685dcb44e104d6a6244b44
Benes Karel <ibenes@fit.vutbr.cz>
2023-12-13 17:57:52 +0100

    Rework model architecture

--- textbite/models/graph/model.py
+++ textbite/models/graph/model.py
@@ -8,6 +8,25 @@ from torch_geometric.nn import GCNConv
 from textbite.models.graph.create_graphs import Graph
 
 
+class Block(torch.nn.Module):
+    def __init__(
+        self,
+        width,
+        dropout_prob,
+    ):
+        super().__init__()
+        self.gcn = GCNConv(width, width)
+        self.nonlin = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, x, edge_index):
+        x = self.gcn(x, edge_index)
+        x = self.nonlin(x)
+        x = self.dropout(x)
+
+        return x
+
+
 class GraphModel(torch.nn.Module):
     def __init__(
         self,
@@ -26,25 +45,32 @@ class GraphModel(torch.nn.Module):
         self.output_size = output_size
         self.dropout_prob = dropout_prob
 
-        self.layers = nn.ModuleList()
+        self.in_proj = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.out_proj = torch.nn.Linear(self.hidden_size, self.output_size)
+        self.blocks = nn.ModuleList()
+        for _ in range(n_layers):
+            self.blocks.append(Block(self.hidden_size, dropout_prob))
 
-        if n_layers == 1:
-            self.layers.append(GCNConv(self.input_size, self.output_size))
-        else:
-            self.layers.append(GCNConv(self.input_size, hidden_size))
-            self.layers.append(nn.ReLU())
-            self.layers.append(nn.Dropout(self.dropout_prob))
-
-            for _ in range(n_layers - 2):
-                self.layers.append(GCNConv(hidden_size, hidden_size))
-                self.layers.append(nn.ReLU())
-                self.layers.append(nn.Dropout(self.dropout_prob))
-
-            self.layers.append(GCNConv(hidden_size, output_size))
+        # self.layers = nn.ModuleList()
+        # if n_layers == 1:
+        #     self.layers.append(GCNConv(self.input_size, self.output_size))
+        # else:
+        #     self.layers.append(GCNConv(self.input_size, hidden_size))
+        #     self.layers.append(nn.ReLU())
+        #     self.layers.append(nn.Dropout(self.dropout_prob))
+        #
+        #     for _ in range(n_layers - 2):
+        #         self.layers.append(GCNConv(hidden_size, hidden_size))
+        #         self.layers.append(nn.ReLU())
+        #         self.layers.append(nn.Dropout(self.dropout_prob))
+        #
+        #     self.layers.append(GCNConv(hidden_size, output_size))
 
     def forward(self, x, edge_index):
-        for layer in self.layers:
-            x = layer(x, edge_index) if isinstance(layer, GCNConv) else layer(x)
+        x = self.in_proj(x)
+        for block in self.blocks:
+            x = x + block(x, edge_index)
+        x = self.out_proj(x)
 
         return x
 
